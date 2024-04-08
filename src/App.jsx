bf7dfbc37d84cba9af0e2c527f7e68a1985ae57a
David Sapiro <dsap48@gmail.com>
2024-04-08 13:34:52 -0700

    get dogs

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,32 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './Nav';
 import RoutesList from './RoutesList';
+import { useState } from 'react';
 
 function App() {
+  const [dogsFetched, setDogsFetched] = useState(false);
+  const [dogData, setDogData] = useState([]);
+
+  async function getDogs() {
+    const response = await fetch("http://localhost:5001/dogs");
+    const data = await response.json();
+    setDogsFetched(true);
+    setDogData(data);
+  }
+
+  if (!dogsFetched) getDogs();
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Nav dogs={{}} />
-        <RoutesList />
-      </BrowserRouter>
+    <div>
+    {dogsFetched
+      ? <div className="App">
+          <BrowserRouter>
+            <Nav dogs={dogData} />
+            <RoutesList />
+          </BrowserRouter>
+        </div>
+      : <h1>Loading...</h1>
+    }
     </div>
   );
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 
 function Nav({ dogs }) {
-    <ul>
 
-    </ul>;
+    const dogLinks = dogs.map(dog =>
+        <Link to={`/dogs/${dog.name}`} key={uuid()}>{dog.name}</Link>
+    )
+
+    return (
+    <div>
+        {dogLinks}
+    </div>
+    )
 }
 
 export default Nav;
